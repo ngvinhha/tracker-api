b9d35702e68d6452fdb74de278a8cf6562472394
ngvinhha <55776929+ngvinhha@users.noreply.github.com>
2021-07-20 10:07:21 +0700

    Update api_handler.js

--- api_handler.js
+++ api_handler.js
@@ -34,6 +34,8 @@ const server = new ApolloServer({
   typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
   resolvers,
   context: getContext,
+  playground: true,
+  introspection: true,
   formatError: (error) => {
     console.log(error);
     return error;
